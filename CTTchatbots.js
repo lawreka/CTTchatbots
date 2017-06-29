9c8e39a6d3acb18a12f868bf8a3d45213eb8441f
Kathryn Isabelle Lawrence <lawr3ka@gmail.com>
2017-06-29 19:44:14 +0200

    framework for chatbots scrollytelling
    still to do:
    - make images for backgrounds that work
    - add classmates' story contributions
    - make waypoints to interject classmate contributions

---
+++ CTTchatbots.js
@@ -0,0 +1,107 @@
+var waypoint = new Waypoint({
+  element: $("#top"),
+  handler: function(){
+    $("#theimage").addClass("hidden");
+  }
+});
+var waypoint = new Waypoint({
+  element: $("#text1"),
+  handler: function(){
+    $("#theimage").removeClass("hidden");
+    $("#theimage").addClass("position2");
+    $("#theimage").empty();
+    $("#theimage").append("<img class='imgsize'  src='imgs/ELIZA.png'>");
+  }
+});
+var waypoint = new Waypoint({
+  element: $("#text2"),
+  handler: function(){
+    $("#theimage").empty();
+    $("#theimage").append("<img class='imgsize'  src='imgs/Jabberwacky.png'>");
+  }
+});
+var waypoint = new Waypoint({
+  element: $("#text3"),
+  handler: function(){
+    $("#theimage").empty();
+    $("#theimage").append("<img class='imgsize'  src='imgs/ALICE.jpg'>");
+  }
+});
+var waypoint = new Waypoint({
+  element: $("#text4"),
+  handler: function(){
+    $("#theimage").empty();
+    $("#theimage").append("<img class='imgsize'  src='imgs/SmarterChild.png')>");
+  }
+});
+var waypoint = new Waypoint({
+  element: $("#text5"),
+  handler: function(){
+    $("#theimage").empty();
+    $("#theimage").append("<img class='imgsize'  src='imgs/APPSTORE.png'>");
+    $("#theimage").append("<img class='imgsize'  src='imgs/Cleverbot_Logo.jpg'>");
+  }
+});
+var waypoint = new Waypoint({
+  element: $("#text6"),
+  handler: function(){
+    $("#theimage").empty();
+    $("#theimage").append("<img class='imgsize'  src='imgs/WhatsApp.png'>");
+    $("#theimage").append("<img class='imgsize'  src='imgs/Kik.png'>");
+    $("#theimage").append("<img class='imgsize'  src='imgs/Siri.png'>");
+  }
+});
+var waypoint = new Waypoint({
+  element: $("#text7"),
+  handler: function(){
+    $("#theimage").empty();
+    $("#theimage").append("<img class='imgsize'  src='imgs/WeChat.png'>");
+    $("#theimage").append("<img class='imgsize'  src='imgs/Line.png'>");
+    $("#theimage").append("<img class='imgsize'  src='imgs/Snapchat.png'>");
+    $("#theimage").append("<img class='imgsize'  src='imgs/FBmessenger.png'>");
+    $("#theimage").append("<img class='imgsize'  src='imgs/iMessage.png'>");
+    $("#theimage").append("<img class='imgsize'  src='imgs/IBMWatson.png'>");
+  }
+});
+var waypoint = new Waypoint({
+  element: $("#text8"),
+  handler: function(){
+    $("#theimage").empty();
+    $("#theimage").append("<img class='imgsize'  src='imgs/Mitsuku.jpg'>");
+}
+});
+var waypoint = new Waypoint({
+  element: $("#text9"),
+  handler: function(){
+    $("#theimage").empty();
+    $("#theimage").append("<img class='imgsize'  src='imgs/AmazonAlexa.png'>");
+}
+});
+var waypoint = new Waypoint({
+  element: $("#text10"),
+  handler: function(){
+    $("#theimage").empty();
+    $("#theimage").append("<img class='imgsize'  src='imgs/Rose.jpg'>");
+}
+});
+var waypoint = new Waypoint({
+  element: $("#text11"),
+  handler: function(){
+    $("#theimage").empty();
+    $("#theimage").append("<img class='imgsize'  src='imgs/WhatsApp.png'>");
+    $("#theimage").append("<img class='imgsize'  src='imgs/FBmessenger.png'>");
+    $("#theimage").append("<img class='imgsize'  src='imgs/Line.png'>");
+    $("#theimage").append("<img class='imgsize'  src='imgs/Viber.png'>");
+    $("#theimage").append("<img class='imgsize'  src='imgs/kakaotalk.png'>");
+    $("#theimage").append("<img class='imgsize'  src='imgs/WeChat.png'>");
+    $("#theimage").append("<img class='imgsize'  src='imgs/Tay AI.jpg'>");
+  }
+});
+var waypoint = new Waypoint({
+  element: $("#text12"),
+  handler: function(){
+    $("#theimage").empty();
+    $("#theimage").append("<img class='imgsize'  src='imgs/Bot.png'>");
+    $("#theimage").append("<img class='imgsize'  src='imgs/Chat.png'>");
+  }
+});
